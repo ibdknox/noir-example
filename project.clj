9def9e6a68702d122f17a9b8c52b8bdede6f8777
Chris Granger <ibdknox@gmail.com>
2011-05-23 22:42:34 -0700

    move to the next noir version
    Signed-off-by: Chris Granger <ibdknox@gmail.com>

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject noir-example "0.1.0"
             :description "An example of a noir project"
             :dependencies [[org.clojure/clojure "1.2.1"]
-                           [noir "0.2.3"]]
+                           [noir "0.2.4"]]
             :main noir-example.core)
